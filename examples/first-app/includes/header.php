074433f9b301f7e912bfe537ed82fd1495011cf9
Mark Conroy <info@adesignforlife.net>
2015-06-29 20:58:53 +0100

    proof of concept annertech app

---
+++ examples/first-app/includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title> Annertech's App </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css">
+    <script src="https://code.jquery.com/jquery-1.8.2.min.js"></script>
+    <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+    <link rel="apple-touch-icon" href="img/tutsTouchIcon.png" />
+</head>
