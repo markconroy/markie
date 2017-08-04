47f2d186d1cca5f2aefe440f558059180990188c
Mark Conroy <info@adesignforlife.net>
2017-08-04 13:52:47 +0100

    back to es6 version

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,34 @@
-var CACHE = 'markie';
+var cacheName = 'markie_cache';
 
-var item_1 = '/articles/web-design/should-i-add-slider-or-carousel-my-website';
+// TODO create variables for the first 5 articles, get them on install, then
+// check for updates when you next view the app.
+// e.g. var item_1 = '/articles/web-design/conversation-i-have-nearly-every-designer';
 
-self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
-  evt.waitUntil(precache());
+self.addEventListener('install', e => {
+  // once the SW is installed, go ahead and fetch the resources
+  // to make this work offline
+  e.waitUntil(
+    caches.open(markie_cache).then(cache => {
+      return cache.addAll([
+        '/',
+        '/about',
+        '/articles',
+        '/articles/drupal/simple-plan-everyone-get-free-t-shirt-drupalcon',
+        '/pwa/offline.css'
+      ]).then(() => self.skipWaiting());
+    })
+  );
 });
 
-self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
-  evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
-    return fromCache(evt.request);
-  }));
+// when the browser fetches a url, either respond with
+// the cached object or go ahead and fetch the actual url
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // ensure we check the *right* cache to match against
+    caches.open(markie_cache).then(cache => {
+      return cache.match(event.request).then(res => {
+        return res || fetch(event.request)
+      });
+    })
+  );
 });
-
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll([
-      '/',
-      '/about',
-      '/articles',
-      '/articles/drupal/simple-plan-everyone-get-free-t-shirt-drupalcon',
-      '/articles/nevermind-25-years-old-today',
-      item_1
-    ]);
-  });
-}
-
-function fromNetwork(request, timeout) {
-  return new Promise(function (fulfill, reject) {
-    var timeoutId = setTimeout(reject, timeout);
-    fetch(request).then(function (response) {
-      clearTimeout(timeoutId);
-      fulfill(response);
-    }, reject);
-  });
-}
-
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
