8031a2ce6b71aa8877489f1630525f097b9a4600
Mark Conroy <info@adesignforlife.net>
2017-08-04 11:02:45 +0100

    arrrgh

--- pwa/custom.js
+++ pwa/custom.js
@@ -1,13 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js', {scope: './'}).then(function(registration) {
-    document.querySelector('#status').textContent = 'succeeded';
-  }).catch(function(error) {
-    document.querySelector('#status').textContent = error;
-  });
-} else {
-  // The current browser doesn't support service workers.
-  var aElement = document.createElement('a');
-  aElement.href = 'http://www.chromium.org/blink/serviceworker/service-worker-faq';
-  aElement.textContent = 'unavailable';
-  document.querySelector('#status').appendChild(aElement);
-}
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
