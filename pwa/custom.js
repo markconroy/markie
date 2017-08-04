322631afdf7f496961454725987f12e99111c1a3
Mark Conroy <info@adesignforlife.net>
2017-08-04 11:10:41 +0100

    moving again

--- pwa/custom.js
+++ pwa/custom.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('./service-worker.js')
+    .register('service-worker.js')
     .then(function() {
       console.log('Service Worker Registered');
     });
