aa44386dd527d57c6274dc1e29b4ef1ca314aa9c
Mark Conroy <info@adesignforlife.net>
2017-08-04 10:02:24 +0100

    adding pwa, I think

---
+++ pwa/app.js
@@ -0,0 +1,6 @@
+  UpUp.start({
+    'content-url': 'home', // show this when the user is offline
+    'assets': [ // define additional assets needed while offline:
+      'css/offline.css' // custom stylesheets,
+    ]
+  });
