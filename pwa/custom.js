fad131947fde3d6b2157d599312b9ec8d7b66442
Mark Conroy <info@adesignforlife.net>
2017-08-04 11:06:38 +0100

    coding std

--- pwa/custom.js
+++ pwa/custom.js
@@ -1,5 +1,7 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
+  navigator.serviceWorker
+    .register('./service-worker.js')
+    .then(function() {
+      console.log('Service Worker Registered');
+    });
+}
