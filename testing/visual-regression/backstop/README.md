d5f7f8cbcf517110855dcdc45fcdd46c7054e51f
Mark Conroy <info@adesignforlife.net>
2018-01-09 19:59:14 +0000

    adding backstopjs for regression testing

---
+++ testing/visual-regression/backstop/README.md
@@ -0,0 +1,18 @@
+# BackstopJS
+
+## Instructions for Set Up for First Time (new projects)
+- Install backstopjs globally
+-- `npm install -g backstopjs`
+- Initialise backstop
+-- `backstop init`
+- Set up the tests
+-- Edit backstop.json
+- Run the references
+-- backstop reference
+- Run the tests
+-- backstop test
+- Write more tests
+
+# Something not working?
+You probably need a more recent version of node
+- nvm install 8
---
+++ testing/visual-regression/backstop/backstop_data/html_report/config.js
@@ -0,0 +1,165 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/markie_Global_Items_0_above-header_0_phone.png",
+        "test": "../bitmaps_test/20180109-194148/markie_Global_Items_0_above-header_0_phone.png",
+        "selector": ".above-header",
+        "fileName": "markie_Global_Items_0_above-header_0_phone.png",
+        "label": "Global Items",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/markie_Global_Items_1_header_0_phone.png",
+        "test": "../bitmaps_test/20180109-194148/markie_Global_Items_1_header_0_phone.png",
+        "selector": "header",
+        "fileName": "markie_Global_Items_1_header_0_phone.png",
+        "label": "Global Items",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/markie_Global_Items_2_footerfooter_0_phone.png",
+        "test": "../bitmaps_test/20180109-194148/markie_Global_Items_2_footerfooter_0_phone.png",
+        "selector": "footer.footer",
+        "fileName": "markie_Global_Items_2_footerfooter_0_phone.png",
+        "label": "Global Items",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/markie_Global_Items_3_credits_0_phone.png",
+        "test": "../bitmaps_test/20180109-194148/markie_Global_Items_3_credits_0_phone.png",
+        "selector": ".credits",
+        "fileName": "markie_Global_Items_3_credits_0_phone.png",
+        "label": "Global Items",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/markie_Global_Items_0_above-header_1_tablet.png",
+        "test": "../bitmaps_test/20180109-194148/markie_Global_Items_0_above-header_1_tablet.png",
+        "selector": ".above-header",
+        "fileName": "markie_Global_Items_0_above-header_1_tablet.png",
+        "label": "Global Items",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/markie_Global_Items_1_header_1_tablet.png",
+        "test": "../bitmaps_test/20180109-194148/markie_Global_Items_1_header_1_tablet.png",
+        "selector": "header",
+        "fileName": "markie_Global_Items_1_header_1_tablet.png",
+        "label": "Global Items",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/markie_Global_Items_2_footerfooter_1_tablet.png",
+        "test": "../bitmaps_test/20180109-194148/markie_Global_Items_2_footerfooter_1_tablet.png",
+        "selector": "footer.footer",
+        "fileName": "markie_Global_Items_2_footerfooter_1_tablet.png",
+        "label": "Global Items",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/markie_Global_Items_3_credits_1_tablet.png",
+        "test": "../bitmaps_test/20180109-194148/markie_Global_Items_3_credits_1_tablet.png",
+        "selector": ".credits",
+        "fileName": "markie_Global_Items_3_credits_1_tablet.png",
+        "label": "Global Items",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
