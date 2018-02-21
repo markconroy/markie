af3a097ac32d4ad35fc6d209f224dd12cdc01bed
Mark Conroy <info@adesignforlife.net>
2018-02-21 18:51:37 +0000

    updating admin_toolbar

--- web/modules/contrib/admin_toolbar/js/admin_toolbar.js
+++ web/modules/contrib/admin_toolbar/js/admin_toolbar.js
@@ -1,10 +1,10 @@
-(function ($) {
+(function ($, Drupal) {
   Drupal.behaviors.adminToolbar = {
     attach: function (context, settings) {
 
       $('a.toolbar-icon', context).removeAttr('title');
-  
-      $('.toolbar-tray-horizontal li.menu-item--expanded, .toolbar-tray-horizontal ul li.menu-item--expanded .menu-item', context).hoverIntent({
+
+      $('.toolbar-tray li.menu-item--expanded, .toolbar-tray ul li.menu-item--expanded .menu-item', context).hoverIntent({
         over: function () {
           // At the current depth, we should delete all "hover-intent" classes.
           // Other wise we get unwanted behaviour where menu items are expanded while already in hovering other ones.
@@ -16,13 +16,13 @@
         },
         timeout: 250
       });
-  
+
       // Make the toolbar menu navigable with keyboard.
       $('ul.toolbar-menu li.menu-item--expanded a', context).on('focusin', function () {
         $('li.menu-item--expanded', context).removeClass('hover-intent');
         $(this).parents('li.menu-item--expanded').addClass('hover-intent');
       });
-  
+
       $('ul.toolbar-menu li.menu-item a', context).keydown(function (e) {
         if ((e.shiftKey && (e.keyCode || e.which) == 9)) {
           if ($(this).parent('.menu-item').prev().hasClass('menu-item--expanded')) {
@@ -30,15 +30,15 @@
           }
         }
       });
-  
+
       $('.toolbar-menu:first-child > .menu-item:not(.menu-item--expanded) a, .toolbar-tab > a', context).on('focusin', function () {
         $('.menu-item--expanded').removeClass('hover-intent');
       });
-  
+
       $('.toolbar-menu:first-child > .menu-item', context).on('hover', function () {
-        $(this,'a').css("background: #fff;");
+        $(this, 'a').css("background: #fff;");
       });
-  
+
       $('ul:not(.toolbar-menu)', context).on({
         mousemove: function () {
           $('li.menu-item--expanded').removeClass('hover-intent');
@@ -50,4 +50,4 @@
 
     }
   };
-})(jQuery);
+})(jQuery, Drupal);
