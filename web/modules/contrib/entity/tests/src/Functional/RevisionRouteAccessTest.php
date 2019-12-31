a7985952c81ed65d4d8d017650686b8895563d1f
Mark Conroy <mark@mark.ie>
2019-12-31 13:03:22 +0000

    updating drupal module entity

--- web/modules/contrib/entity/tests/src/Functional/RevisionRouteAccessTest.php
+++ web/modules/contrib/entity/tests/src/Functional/RevisionRouteAccessTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests\entity\Functional;
 
 use Drupal\entity_module_test\Entity\EnhancedEntity;
-use Drupal\entity_module_test\Entity\EnhancedEntityBundle;
 use Drupal\Tests\block\Traits\BlockCreationTrait;
 use Drupal\Tests\BrowserTestBase;
 
@@ -39,11 +38,6 @@ class RevisionRouteAccessTest extends BrowserTestBase {
    */
   protected function setUp() {
     parent::setUp();
-
-    EnhancedEntityBundle::create([
-      'id' => 'default',
-      'label' => 'Default',
-    ])->save();
 
     $this->placeBlock('local_tasks_block');
     $this->placeBlock('system_breadcrumb_block');
